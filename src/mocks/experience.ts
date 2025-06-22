b4f9d31b288056fb447754f0221e8b6296151505
OussamaAtifii <oussamaati03@gmail.com>
2025-06-22 15:13:37 +0200

    feat: add experience mock data and type definition

---
+++ src/mocks/experience.ts
@@ -0,0 +1,17 @@
+import type { Experience } from '@customTypes/experience';
+
+export const experience: Experience[] = [
+  {
+    title: 'FullStack Developer',
+    company: 'Magical Analytics',
+    companyLink: 'https://magical.es/',
+    date: 'June 2024 - Present',
+    description: [
+      'Developed and maintained backend services using Laravel and MySQL to automate and manage data, improving clients internal process speed and reducing manual errors.',
+      'Implemented REST APIs and developed custom dashboards for real-time data presentation, incorporating modular and adaptable structures.',
+      'Built automated workflows in HubSpot using Node.js, integrating custom logic and managing processes within the CRM ecosystem.',
+      'Designed and developed dynamic websites with WordPress, Elementor, and WooCommerce.',
+      'Created custom WordPress plugins, tailoring specific functionalities to clients’ needs.',
+    ],
+  },
+];
