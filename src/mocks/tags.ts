a74ebed420074034ca125fd1be858290149c606f
OussamaAtifii <oussamaati03@gmail.com>
2025-06-22 19:22:57 +0200

    feat: add tags data

---
+++ src/mocks/tags.ts
@@ -0,0 +1,13 @@
+import LanguageCertIcon from '@icons/LanguageCertIcon.astro';
+import UdemyIcon from '@icons/UdemyIcon.astro';
+
+export const tags = {
+  UDEMY: {
+    name: 'Udemy',
+    icon: UdemyIcon,
+  },
+  LANGUAGE_CERT: {
+    name: 'Language Cert',
+    icon: LanguageCertIcon,
+  },
+};
