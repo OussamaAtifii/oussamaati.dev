6f2b21a4edc83b5970def35b0d4c1c829e8991ec
OussamaAtifii <oussamaati03@gmail.com>
2025-09-08 23:36:48 +0200

    chore: change experience end date

--- src/mocks/experience.ts
+++ src/mocks/experience.ts
@@ -5,7 +5,7 @@ export const experience: Experience[] = [
     title: 'FullStack Developer',
     company: 'Magical Analytics',
     companyLink: 'https://magical.es/',
-    date: 'June 2024 - Present',
+    date: 'June 2024 - September 2025',
     description: [
       'Developed and maintained backend services using Laravel and MySQL to automate and manage data, improving clients internal process speed and reducing manual errors.',
       'Implemented REST APIs and developed custom dashboards for real-time data presentation, incorporating modular and adaptable structures.',
